1e20c055164fdeab43090edeabae3e85496ac9fb
Aaron Alonso Torrens <aaronalonsotorrens@gmail.com>
2024-10-17 20:16:36 +0000

    Style circle and cross for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 const board = document.querySelector("#board")
+const infoDisplay = document.querySelector("#info")
 const createCells = ["","","","","","","","",""]
+
+infoDisplay.textContent = "Mario goes first"
 
 function createBoard() {
     createCells.forEach((index) => {
         const cellElement = document.createElement("div")
         cellElement.classList.add("square")
-
+        const marioElement = document.createElement("div")
+        marioElement.classList.add("mario")
+        cellElement.append(marioElement)
         board.append(cellElement)
 
     })
