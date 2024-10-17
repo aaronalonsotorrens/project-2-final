8dc55712479cd24f320324b16a0ff2e43e285bc7
Aaron Alonso Torrens <aaronalonsotorrens@gmail.com>
2024-10-17 21:04:13 +0000

    Add function to check winning conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,10 @@ function startGame(e) {
 
 }
 
+function checkScore() {
+    const allSquares = document.querySelectorAll(".square")
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
