4131ad5dca0dd5d5a2b946b99146babffb3aee06
Aaron Alonso Torrens <aaronalonsotorrens@gmail.com>
2024-10-24 23:47:48 +0000

    Add a function on game 2 to count each correct and incorrect score, add a countdown timer function

--- assets/js/mario-shadow-quest.js
+++ assets/js/mario-shadow-quest.js
@@ -5,33 +5,69 @@ const gameData = [
     { image: './assets/images/toad-shadow.jpg', options: ['Peach', 'Toad'], correctOption: 1 }
 ];
 
+const timeLeft = document.querySelector('.time-left')
+
 let imageIndex;
+let currentTime = 10
+let timerId = null
 
 function loadNewImage() {
     imageIndex = Math.floor(Math.random() * gameData.length);
 
     const gameImage = document.getElementById('shadowImage');
     shadowImage.src = gameData[imageIndex].image;
+
+    //Include text in the button
+    document.getElementById('option1').textContent = gameData[imageIndex].options[0];
+    document.getElementById('option2').textContent = gameData[imageIndex].options[1];
+
 }
 //Function to check if the option is correct
 function checkOption(optionIndex) {
 
     const resultMessage = document.getElementById('resultMessage');
 
-    const correctOption = gameData[currentImageIndex].correctOption;
+    const correctOption = gameData[imageIndex].correctOption;
 
-    if (gameData[imageIndex].options[optionIndex] === gameData[imageIndex].options[correctOption]) {
+    if (optionIndex === correctOption) {
         resultMessage.textContent = "Correct Well done";
         resultMessage.style.color = "green";
+        incrementScore()
     } else {
         resultMessage.textContent = "Wrong! Try again.";
         resultMessage.style.color = "red";
+        incrementWrongAnswer()
     }
 
     // Time needed to a load a new image
-    setTimeout(loadNewImage, 1500);
+    setTimeout(loadNewImage, 500);
 }
 
-console.log(checkOption)
+//Get the current score and increment it by 1
+
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerHTML = ++oldScore;
+}
+
+//Get the total number of incorrect scores and increment it by 1
+
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerHTML = ++oldScore;
+}
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('Your final score is ' + document.getElementById("score").innerText);
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
 
 window.onload = loadNewImage;
